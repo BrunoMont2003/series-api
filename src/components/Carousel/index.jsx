09279e453fb2842e07d83cd07019dfae8d308ab0
brunomont <brunoadrian.mont@gmail.com>
2022-05-18 18:56:23 -0500

    add: carousel finished

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -3,10 +3,23 @@ import './style.scss'
 import { motion } from 'framer-motion'
 import { CardPrimary } from '../CardPrimary'
 export const Carousel = ({ shows = [] }) => {
+  const [width, setWidth] = useState(0)
+  const carousel = useRef()
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+  }, [])
   return (
     <section className='container'>
-      <motion.div className='carousel pt-10'>
-        <motion.div drag='x' className='inner-carousel py-5'>
+      <motion.div
+        ref={carousel}
+        className='carousel pt-10'
+        whileTap={{ cursor: 'grabbing' }}
+      >
+        <motion.div
+          drag='x'
+          dragConstraints={{ right: 0, left: -width }}
+          className='inner-carousel py-5'
+        >
           {shows.map((show, index) => (
             <CardPrimary key={index} show={show} />
           ))}
