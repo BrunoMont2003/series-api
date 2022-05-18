d4eaa1f368b085e781933365fc26b9c3f8b8b597
brunomont <brunoadrian.mont@gmail.com>
2022-05-17 20:38:49 -0500

    add: sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
-import './styles/globals.css'
+import './styles/globals.scss'
 const root = document.getElementById('root')
 const container = createRoot(root)
 container.render(
