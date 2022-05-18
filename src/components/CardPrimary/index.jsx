22afeb04006320f5b098cb8c966464005363cbb1
brunomont <brunoadrian.mont@gmail.com>
2022-05-18 17:03:55 -0500

    add: star component && creating the card component

--- src/components/CardPrimary/index.jsx
+++ src/components/CardPrimary/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './style.scss'
+import ReactStars from 'react-rating-stars-component'
 
-export const CardPrimary = ({ show: { image } }) => {
+export const CardPrimary = ({ show: { image, name, rating, summary } }) => {
+  const truncateText = (text = '', maxLength = 300) => {
+    // clean html tags
+    text = text.replace(/<\/?[^>]+(>|$)/g, '')
+    if (text.length > maxLength) {
+      text = text.substring(0, maxLength) + '...'
+    }
+    return text
+  }
   return (
-    <figure>
-      <img src={image.original} alt='hola' />
-    </figure>
+    <article className='card'>
+      <figure>
+        <img src={image.original} alt='hola' />
+      </figure>
+      <div className='my-5 mr-10'>
+        <h2 className='font-bold text-xl'>{name}</h2>
+        <ReactStars edit={false} value={rating.average / 2} isHalf />
+        <p className='text-justify my-5'>{truncateText(summary)}</p>
+        <ul className='border-y p-5' />
+      </div>
+    </article>
   )
 }
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -5,8 +5,8 @@ import { CardPrimary } from '../CardPrimary'
 export const Carousel = ({ shows = [] }) => {
   return (
     <section className='container'>
-      <motion.div className='carousel'>
-        <motion.div drag='x' className='inner-carousel'>
+      <motion.div className='carousel pt-10'>
+        <motion.div drag='x' className='inner-carousel py-5'>
           {shows.map((show, index) => (
             <CardPrimary key={index} show={show} />
           ))}
