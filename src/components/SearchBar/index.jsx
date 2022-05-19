ed3902dc8b8b43992c3ae35f6564fe3d5599de02
brunomont <brunoadrian.mont@gmail.com>
2022-05-19 17:48:31 -0500

    mod: fixing some responsive prob

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import './style.scss'
 export const SearchBar = ({ onSubmit, onChange }) => {
   return (
-    <form className='flex items-center searcher gap-5' onSubmit={onSubmit}>
+    <form className='flex flex-col sm:flex-row items-center searcher gap-5' onSubmit={onSubmit}>
       <label htmlFor='simple-search' className='sr-only'>Search</label>
       <div className='relative'>
         <motion.input whileFocus={{ scale: 1.05 }} type='text' onChange={onChange} className='bg-transparent border border-gray-300 text-slate-100 text-sm rounded-lg  block w-full pl-5 p-2.5  ' placeholder='Search by Name' required />
--- src/pages/Shows/index.jsx
+++ src/pages/Shows/index.jsx
@@ -7,25 +7,31 @@ import { getShowsByQuestion } from '../../services/series'
 export const Shows = () => {
   const [question, setQuestion] = useState('')
   const [showsFinded, setShowsFinded] = useState([])
-  const searchOnChange = ({ target: { value } }) => {
+  const searchOnChange = async ({ target: { value } }) => {
     setQuestion(value)
+    const result = await getShowsByQuestion(question)
+    setShowsFinded(result)
   }
   const searchOnSubmit = async (e) => {
     e.preventDefault()
     const result = await getShowsByQuestion(question)
     setShowsFinded(result)
-    console.log(result)
   }
   return (
     <Layout>
-      <section className='flex gap-5 flex-col justify-center items-center search-container min-h-screen w-full'>
-        <h1 className='text-3xl text-center mb-24'>Looking for a specific show?</h1>
-        <SearchBar onChange={(e) => searchOnChange(e)} value={question} onSubmit={(e) => { searchOnSubmit(e) }} />
-        <div className=' flex container justify-center sm:justify-start flex-wrap gap-5 py-5'>
-          {
+      <section className='flex gap-5 flex-col justify-center items-center search-container min-h-screen w-full  bg-red-500'>
+        <article className='h-96 flex flex-col justify-center items-center py-40'>
+          <h1 className='text-3xl text-center mb-24'>Looking for a specific show?</h1>
+          <SearchBar onChange={async (e) => await searchOnChange(e)} value={question} onSubmit={async (e) => { await searchOnSubmit(e) }} />
+        </article>
+        <article>
+          <div className=' flex container justify-center flex-wrap gap-5 py-5'>
+            {
             showsFinded.length > 0 && showsFinded.map(({ show }, index) => <CardSecondary show={show} key={index} />)
           }
-        </div>
+            {showsFinded.length === 0 && <p>No shows found</p>}
+          </div>
+        </article>
       </section>
 
     </Layout>
